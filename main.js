625bb4ebeb413e273b2e7e3d5b7f171068a58058
Mirajaouadi <100517615+Mirajaouadi@users.noreply.github.com>
2023-07-23 09:28:48 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,71 @@
+
+      function calzero() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "0";
+      }
+      function calone() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "1";
+       }
+      function caltow() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "2";
+      }
+      function calthre() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "3";
+      }
+
+      function plus() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "+";
+      }
+      function calfour() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "4";
+      }
+      function calfive() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "5";
+      }
+      function calsix() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "6";
+      }
+      function calsev() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "7";
+      }
+      function caleight() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "8";
+      }
+
+      function calnine() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "9";
+      }
+      function min() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "-";
+      }
+      function div() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "/";
+      }
+
+      function mul() {
+        var res = document.getElementById("res").value;
+        document.getElementById("res").value = res + "*";
+      }
+      function clearr() {
+        document.getElementById("res").value = "";
+      }
+
+      function res() {
+        var x = document.getElementById("res").value;
+        var y = document.getElementById("res");
+        x = eval(x);
+        y.value = x;
+      }
+   
